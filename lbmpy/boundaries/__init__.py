d64a8603c0297a84d706a3583dd8004936b60516
Reza <133172836+ryousofvand@users.noreply.github.com>
2023-07-17 13:34:11 -0500

    Add Neumann with specific flux to boundary conditions

--- lbmpy/boundaries/__init__.py
+++ lbmpy/boundaries/__init__.py
@@ -1,8 +1,8 @@
 from lbmpy.boundaries.boundaryconditions import (
     UBB, FixedDensity, DiffusionDirichlet, SimpleExtrapolationOutflow,
-    ExtrapolationOutflow, NeumannByCopy, NoSlip, StreamInConstant, FreeSlip)
+    ExtrapolationOutflow, NeumannFlux, NeumannByCopy, NoSlip, StreamInConstant, FreeSlip)
 from lbmpy.boundaries.boundaryhandling import LatticeBoltzmannBoundaryHandling
 
 __all__ = ['NoSlip', 'FreeSlip', 'UBB', 'SimpleExtrapolationOutflow', 'ExtrapolationOutflow',
-           'FixedDensity', 'DiffusionDirichlet', 'NeumannByCopy',
+           'FixedDensity', 'DiffusionDirichlet', 'NeumannFlux', 'NeumannByCopy',
            'LatticeBoltzmannBoundaryHandling', 'StreamInConstant']
